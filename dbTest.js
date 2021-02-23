145993663fae5b7ccedc9bb53563428457007d33
carmenlop <cllopez92@gmail.com>
2021-02-23 13:36:17 -0800

    db and models created and working with many to many functionality

---
+++ dbTest.js
@@ -0,0 +1,23 @@
+const db = require('./models')
+
+// db.pokemon.create({
+//     name: 'pikachu',
+//     imageUrl: 'urltest',
+//     hp: 50,
+//     attack: 50, 
+//     defense: 50,
+// }).then(function(pokemon) {
+//     console.log(pokemon.id)
+// })
+
+// db.pokemon.findOne({
+//     where: { id: 1 }
+// }).then(function(foundPokemon) {
+//     db.user.findOne({
+//         where: { id: 1 }
+//     }).then(function(foundUser) {
+//         foundPokemon.addUser(foundUser).then(function(){
+//             console.log('done!')
+//         })
+//     })
+// })

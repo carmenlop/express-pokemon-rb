ef23046a07355446b9e18d0d7a9dbc981de478b6
carmenlop <cllopez92@gmail.com>
2021-02-25 16:27:40 -0800

    delte functionality and routes working correctly

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,9 +1,10 @@
 const db = require('../models');
 const express = require ("express")
 const router = express.Router()
+const methodOverride = require('method-override')
 router.use(express.static(__dirname + "/public/"))
 const isLoggedIn = require('../middleware/isLoggedIn')
-// router.use(methodOverride("_method")) 
+router.use(methodOverride("_method")) 
 
 // GET shows current team
 router.get('/', isLoggedIn, (req, res) => {
@@ -53,8 +54,6 @@ router.get('/edit/:id', isLoggedIn, (req, res) => {
             db.pokemon.findOne({
                 where: { id: req.params.id }
             }).then((foundPokemon) => {
-                // console.log(foundPokemons)
-                // console.log(foundPokemon)
                 res.render('userTeam/edit.ejs', { pokemons: foundPokemons, pokemonInfo: foundPokemon })
             })
         })
@@ -72,24 +71,20 @@ router.post('/edit/:id', isLoggedIn, (req, res) => {
         }).then(info => {
             res.redirect('/pokemon')
         })
-
 })
 
 
-
-// delete pokemon
-// router.delete('/:id', isLoggedIn, (req, res) => {
-//     db.user.findOne ({
-//         where: {id: req.user.dataValues.id}
-//     }).then((foundUser) => {
-//         foundUser.getPokemons().then(function(foundPokemons) {
-//     db.usersPokemons.destroy({
-//       where: { id: req.params.id }
-//     }).then(function() {
-//       res.redirect('/')
-//     })
-//   })
-// })  
+// DELETE!!! 
+router.delete('/:id', isLoggedIn, (req, res) => {
+    db.usersPokemons.destroy({
+        where: {
+            userId: req.user.dataValues.id,
+            pokemonId: req.params.id
+        }
+    }).then(info => {
+        res.redirect('/pokemon')
+    })
+})
 
 
 module.exports = router;
